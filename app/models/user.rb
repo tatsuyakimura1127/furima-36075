af1ea0cad9e58c35660568b5106b31c386ea25c3
tatsuyakimura1127 <86278073+tatsuyakimura1127@users.noreply.github.com>
2021-07-21 18:27:22 +0900

    ユーザー管理機能の修正５

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,16 +7,18 @@ class User < ApplicationRecord
   PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i.freeze
   validates_format_of :password, with: PASSWORD_REGEX, message: 'は半角英数字混合で設定してください'
 
-  with_options presence: true, format: { with: /\A[ぁ-んァ-ヶー-龥々]+\z/, message: 'には全角文字を使用してください' } do
-    validates :last_name
-    validates :first_name
-  end
-
-  with_options presence: true, format: { with: /\A[ァ-ヶー－]+\z/, message: 'には全角カタカナを使用してください' } do
-    validates :last_name_kana
-    validates :first_name_kana
+  with_options presence: true do
+    with_options format: { with: /\A[ぁ-んァ-ヶー-龥々]+\z/, message: 'には全角文字を使用してください'  } do
+      validates :last_name
+      validates :first_name
+    end
+    with_options format: { with: /\A[ァ-ヶー－]+\z/ , message: 'には全角カタカナを使用してください' } do
+      validates :last_name_kana
+      validates :first_name_kana
+    end
   end
 
   validates :nickname, presence: true
   validates :birthday, presence: true
 end
+
